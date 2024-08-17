5518cb477a36875f5790d569b4c145d33ac37d09
Jennifer Carvalho <jenniferh7637@gmail.com>
2024-08-17 17:08:12 -0300

    adiciona validação

--- src/alunos/dto/create-aluno.dto.ts
+++ src/alunos/dto/create-aluno.dto.ts
@@ -1,1 +1,22 @@
-export class CreateAlunoDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Max } from 'class-validator';
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+export class CreateAlunoDto {
+  @IsString()
+  nome: string;
+
+  @IsString()
+  endereco: string;
+
+  @IsString()
+  telefone: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNumber()
+  @Max(currentYear - 16)
+  anoNascimento: number;
+}
